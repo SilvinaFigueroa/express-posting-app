3eae4d760f0c42de45d8e597ebcbcad91fb63422
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-06-13 09:46:51 -0700

    Retrieves the comment with the specified id

--- data/comments.js
+++ data/comments.js
@@ -1,10 +1,41 @@
 const comment = [{
 
-    id: "a unique identifier.",
-    userId: "the id of the user that created the comment.",
-    postId: "the id of the post the comment was made on.",
+    id: 1,
+    userId: 1,
+    postId: 1,
+    body: "the text of the comment.",
+
+},
+
+{
+
+    id: 2,
+    userId: 2,
+    postId: 3,
+    body: "the text of the comment.",
+
+},
+
+{
+
+    id: 3,
+    userId: 2,
+    postId: 4,
+    body: "the text of the comment.",
+
+},
+
+
+{
+
+    id: 4,
+    userId: 1,
+    postId: 3,
     body: "the text of the comment.",
 
 }
 
 ]
+
+
+module.exports = comment;
--- routes/comments.js
+++ routes/comments.js
@@ -3,4 +3,24 @@ const router = express.Router()
 
 const users = require('../data/users.js');
 const posts = require('../data/posts.js');
-const posts = require('../data/comments.js');
+const comments = require('../data/comments.js');
+
+//GET route to get all comments
+router.get('/', (req, res) => {
+    res.json(comments);
+});
+
+// GET route to get a comment by ID
+router.get('/:id', (req, res, next) => {
+    // Using the Array.find method to find the user with the same id as the one sent with the request
+    const comment = comments.find((comment) => comment.id == req.params.id);
+    if (comment) {
+        res.json(comment);
+    } else {
+        // If the comment doesn't exist
+        next();
+    }
+});
+
+
+module.exports = router;
